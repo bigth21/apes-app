63fdfdc0b222b21ccb3ffd422cebf5e0824f26ee
bigth21 <taekhyeon.nam@gmail.com>
2025-05-29 19:17:51 +0900

    Add dependency

---
+++ src/main/java/me/bigth/apes/infrastructure/SecurityConfig.java
@@ -0,0 +1,17 @@
+package me.bigth.apes.infrastructure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration(proxyBeanMethods = false)
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+}
