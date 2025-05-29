a89530bffd2a7cb70d5ab53b1a6ab1721f888d85
bigth21 <taekhyeon.nam@gmail.com>
2025-05-29 19:37:31 +0900

    Configure Basic Security Config

--- src/main/java/me/bigth/apes/infrastructure/SecurityConfig.java
+++ src/main/java/me/bigth/apes/infrastructure/SecurityConfig.java
@@ -1,16 +1,32 @@
 package me.bigth.apes.infrastructure;
 
+import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration(proxyBeanMethods = false)
 public class SecurityConfig {
+    private static final String[] STATIC_RESOURCE_PATHS = {"/css/**", "/images/**", "/favicon.ico"};
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(Customizer.withDefaults())
+                .logout(Customizer.withDefaults())
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .sessionManagement(session -> session
+                        .sessionConcurrency(concurrency -> concurrency
+                                .maximumSessions(1)))
                 .authorizeHttpRequests(auth -> auth
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll()
+                        .requestMatchers(HttpMethod.GET, STATIC_RESOURCE_PATHS).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/").permitAll()
                         .anyRequest().authenticated());
         return http.build();
     }
