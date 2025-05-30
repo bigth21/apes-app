3864f46ba53badcbc6ee291b2d279a3acf60bdfb
bigth21 <taekhyeon.nam@gmail.com>
2025-05-30 17:26:03 +0900

    Modify test code for home page

--- src/test/java/me/bigth/apes/interfaces/web/HomeControllerTest.java
+++ src/test/java/me/bigth/apes/interfaces/web/HomeControllerTest.java
@@ -1,8 +1,12 @@
 package me.bigth.apes.interfaces.web;
 
+import me.bigth.apes.infrastructure.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -10,12 +14,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = HomeController.class)
+@WebMvcTest(controllers = HomeController.class, includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class))
 class HomeControllerTest {
     @Autowired
     MockMvc mockMvc;
 
     @Test
+    @WithAnonymousUser
     void home() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
