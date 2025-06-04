fb5d3dd1f86980a39ac7e8b73290b2886f1571a2
bigth21 <taekhyeon.nam@gmail.com>
2025-06-04 18:27:07 +0900

    Separate profiles and create UserAuthority entity

---
+++ src/main/java/me/bigth/apes/core/UserAuthority.java
@@ -0,0 +1,18 @@
+package me.bigth.apes.core;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class UserAuthority extends BaseEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "authorityId", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Authority authority;
+}
