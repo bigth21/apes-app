e325a3d0efc7cd6dd7309b4001ffdf1d5e4e1da9
bigth21 <taekhyeon.nam@gmail.com>
2025-06-10 20:44:25 +0900

    Polishing

--- src/main/java/me/bigth/apes/infrastructure/SecurityConfig.java
+++ src/main/java/me/bigth/apes/infrastructure/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -47,7 +46,7 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepository) {
-        return new DefaultUserDetailsService(userRepository);
+        return new CustomUserDetailsService(userRepository);
     }
 
     @Bean
